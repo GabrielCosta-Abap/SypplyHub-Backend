fe9e175a627eca7592f85ae3dec39ea4255635a4
GabrielCosta-Abap <gabriel.costarosa.9@gmail.com>
2024-04-08 11:49:35 -0300

    ajuste cors

--- src/server.js
+++ src/server.js
@@ -6,14 +6,19 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+    
+    next();
+});
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.use(routes)
-
-app.get('/', (req, res)=>{
-    res.send('hello world')
-
-
-
-})
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
